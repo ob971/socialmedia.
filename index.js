8f44c2b56ad4fcd40d4c2a71639a98e63618e97d
obsa <obsukebede2022@gmail.com>
2024-02-27 14:49:25 +0300

    upadting

--- index.js
+++ index.js
@@ -32,3 +32,22 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+const upload = multer({ storage });
+
+
+/*DB CONNECTION*/
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
